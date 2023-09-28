b46bb48a2dd940c36823ac3c2a1af84dec19535f
Chris Lin <linchr@VI614496-11.local>
2023-09-28 10:38:32 +0800

    [feat] add all after hook example

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,7 +1,7 @@
 import uuid
 from typing import Optional, Dict, Any
 
-from fastapi import Depends, Request
+from fastapi import Depends, Request, Response
 from fastapi_users import BaseUserManager, UUIDIDMixin, FastAPIUsers
 from fastapi_users.db import SQLAlchemyUserDatabase
 
@@ -23,45 +23,39 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
         self, user: User, token: str, request: Optional[Request] = None
     ):
         print(f"User {user.id} has forgot their password. Reset token: {token}")
-        print('[request] ', request.__dict__)
-
 
     async def on_after_request_verify(
         self, user: User, token: str, request: Optional[Request] = None
     ):
         print(f"Verification requested for user {user.id}. Verification token: {token}")
-        print('[request] ', request)
 
     async def on_after_update(
         self, user: User, update_dict: Dict[str, Any], request: Optional[Request] = None
     ):
         print(f"{user}")
         
-    
-    # async def on_after_verify(
-    #     self, user: models.UP, request: Optional[Request] = None
-    # ) -> None:
+    async def on_after_verify(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        print(f"Successful user verification. {user.id}. Verification token: {token}")
 
+    async def on_after_reset_password(
+        self, user: User, request: Optional[Request] = None
+    ):
+        print(f"User {user.id} reset the password")
 
-    # async def on_after_reset_password(
-    #     self, user: models.UP, request: Optional[Request] = None
-    # ) -> None:
-
-    # async def on_after_login(
-    #     self,
-    #     user: models.UP,
-    #     request: Optional[Request] = None,
-    #     response: Optional[Response] = None,
-    # )
+    async def on_after_login(
+        self, user: User, request: Optional[Request] = None, response: Optional[Response] = None,
+    ):
+        print(f"User {user.id} login")
         
-    # async def on_before_delete(
-    #     self, user: models.UP, request: Optional[Request] = None
-    # ) -> None:
+    async def on_before_delete(
+        self, user: User, request: Optional[Request] = None, response: Optional[Response] = None,
+    ):
+        print(f"User {user.id} been deleted")
 
 async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
     yield UserManager(user_db)
 
-
 fastapi_users = FastAPIUsers[User, uuid.UUID](get_user_manager, [auth_backend])
-
 current_active_user = fastapi_users.current_user(active=True)
